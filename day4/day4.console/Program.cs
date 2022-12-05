ddcd706fae9539913c10ad742f9ed7fb2ea8a76f
Andrew <aalkema@gmail.com>
2022-12-05 15:22:19 -0500

    day4

--- day4/day4.console/Program.cs
+++ day4/day4.console/Program.cs
@@ -1,16 +1,48 @@
-﻿public class Program
+﻿namespace day4.console;
+
+public class Program
 {
     private static void Main(string[] args)
     {
         using FileStream fs = File.OpenRead("input.txt");
         using var sr = new StreamReader(fs);
+        int fullyContainedPairs = 0;
+        int overlappingPairs = 0;
+        string line;
         while ((line = sr.ReadLine()) != null) {
-            HandlePairs(line);
+            if (DoesPairFullyContainOther(line)) {
+                fullyContainedPairs++;
+            }
+            if (DoPairsOverlap(line)) {
+                overlappingPairs++;
+            }
         }
-
+        Console.WriteLine(fullyContainedPairs);
+        Console.WriteLine($"Overlapping pairs: {overlappingPairs}");
     }
 
-    public static void DoesPairFullyContainOther(string pair) {
+    public static bool DoesPairFullyContainOther(string pairs) {
+        List<Sections> sections = GetSectionsFromString(pairs);
+        sections.Sort((s1, s2) => s1.Length.CompareTo(s2.Length));
+        return sections[1].Contains(sections[0]);
+    }
 
+    public static bool DoPairsOverlap(string pairs) {
+        List<Sections> sections = GetSectionsFromString(pairs);
+        if ( sections[1].Overlap(sections[0]) ) {
+            Console.WriteLine($"Overlap: {string.Join(",", sections)}");
+        } else {
+            Console.WriteLine($"No overlap: {string.Join(",", sections)}");
+        }
+        return sections[1].Overlap(sections[0]);
+    }
+
+    public static List<Sections> GetSectionsFromString(string pairs) {
+        List<Sections> results = new List<Sections>();
+        string[] sections = pairs.Split(",");
+        foreach ( string section in sections ) {
+            results.Add(new Sections(section));
+        }
+        return results;
     }
 }
