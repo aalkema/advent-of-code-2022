c58b676d9e1101eb0e5a9c3ae8b7e8eb28378ef9
Andrew Alkema <andrewalkema@Andrews-MacBook-Air.local>
2022-12-05 08:38:35 -0500

    day4

---
+++ day4/day4.console/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        using FileStream fs = File.OpenRead("input.txt");
+        using var sr = new StreamReader(fs);
+        while ((line = sr.ReadLine()) != null) {
+            HandlePairs(line);
+        }
+
+    }
+
+    public static void DoesPairFullyContainOther(string pair) {
+
+    }
+}
